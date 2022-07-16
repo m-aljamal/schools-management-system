ca7d546cb44fe293d75ca243159d2d5994f78cc6
m-aljamal <mohammadjamol@gmail.com>
2022-07-16 15:45:33 +0300

    emp levels and divisions

--- client/src/screens/projects/project/Employee.tsx
+++ client/src/screens/projects/project/Employee.tsx
@@ -7,6 +7,20 @@ const Employee = () => {
   return (
     <div>
       <p>{employee?.name}</p>
+      <p className="grid grid-cols-3">
+        {employee?.levels.map(({ id, name, divisions }) => (
+          <div>
+            <p key={id} className="text-green-700 ">
+              {name}
+            </p>
+            <div>
+              {divisions?.map(({ id, name }) => (
+                <p className="text-red-700">{name}</p>
+              ))}
+            </div>
+          </div>
+        ))}
+      </p>
     </div>
   );
 };
